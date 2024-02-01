1b3f56f3cf946f755586ba94fb88bc532cb7e63d
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2024-02-01 09:29:10 +0300

    Initial Code

---
+++ A94164/SOLID_principles.py
@@ -0,0 +1,20 @@
+class Library:
+    def checkout(self, book_id, user_id):
+        # Checkout logic
+        pass
+
+    def return_book(self, book_id, user_id):
+        # Return logic
+        pass
+
+    def add_book(self, book):
+        # Add book logic
+        pass
+
+    def send_overdue_notices(self):
+        # Send notices logic
+        pass
+
+    def log_activity(self, activity):
+        # Logging logic
+        pass
