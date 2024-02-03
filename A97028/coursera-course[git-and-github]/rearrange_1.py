6667041e4b493871f35f12d1f471cdd6fb3fbcba
HassanBahati <mukisabahati@gmail.com>
2024-02-03 23:33:51 +0300

    feat: add function that rearranges a name

---
+++ A97028/coursera-course[git-and-github]/rearrange_1.py
@@ -0,0 +1,13 @@
+# imports the regular expression module
+import re
+
+# function that rearranges the name
+def rearrange_name(name):
+    # using the search method to find the first and last name
+    result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+
+    if result == None:
+        # if the name is not in the correct format, return the name as is
+        return name
+    # if the name is in the correct format, return the rearranged name
+    return "{} {}".format(result[2], result[1])
