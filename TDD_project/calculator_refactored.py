0a1ef33d990a84c598d9dae266682471a0191790
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2024-03-07 09:54:41 +0300

    Revert "modified refactored code"

--- TDD_project/calculator_refactored.py
+++ TDD_project/calculator_refactored.py
@@ -1,72 +1,35 @@
 import math
 
+
 class Calculator:
-    """
-    A simple calculator class for basic arithmetic operations, exponentiation, square root, and absolute value calculation.
-    """
+    # A simple calculator class.
 
     def add(self, x, y):
-        """
-        Adds two numbers.
-        :param x: First number.
-        :param y: Second number.
-        :return: The sum of x and y.
-        """
+        # Adds two numbers.
         return x + y
 
     def subtract(self, x, y):
-        """
-        Subtracts y from x.
-        :param x: First number.
-        :param y: Second number to be subtracted from x.
-        :return: The difference of x and y.
-        """
+        # Subtracts y from x.
         return x - y
 
     def multiply(self, x, y):
-        """
-        Multiplies two numbers.
-        :param x: First number.
-        :param y: Second number.
-        :return: The product of x and y.
-        """
+        # Multiplies two numbers.
         return x * y
 
     def divide(self, x, y):
-        """
-        Divides x by y.
-        :param x: Numerator.
-        :param y: Denominator.
-        :return: The quotient of x and y if y is not zero, otherwise raises a ZeroDivisionError.
-        """
+        # # Divides x by y.
         if y == 0:
-            raise ZeroDivisionError("Division by zero is not allowed.")
+            return "Error: Division by zero"
         return x / y
 
     def exponentiate(self, x, y):
-        """
-        Raises x to the power of y.
-        :param x: Base.
-        :param y: Exponent.
-        :return: x raised to the power of y.
-        """
-        return x ** y
+        # Raises x to the power of y.
+        return x**y
 
     def square_root(self, x):
-        """
-        Calculates the square root of x.
-        :param x: The number to find the square root of.
-        :return: The square root of x. If x is negative, it raises a ValueError.
-        """
-        if x < 0:
-            raise ValueError("Cannot calculate the square root of a negative number.")
+        # Calculates the square root of x.
         return math.sqrt(x)
 
     def absolute_value(self, x):
-        """
-        Calculates the absolute value of x.
-        :param x: The number to find the absolute value of.
-        :return: The absolute value of x.
-        """
+        # Calculates the absolute value of x.
         return abs(x)
-
